1001def189e0584f02ced388b9937df530f93784
valeryharbatouski <112054601+valeryharbatouski@users.noreply.github.com>
2022-09-22 15:27:03 +0400

    init new console project

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ValeryOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car();
+            var plane = new Plane();
+            var ship = new Ship();
+
+            Vehicle[] vehicles = new Vehicle[] {car, plane, ship};
+
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine(vehicle.Go());
+            }
+        }
+    }
+
+
+    public abstract class Vehicle
+    {
+        public abstract string Go();
+    }
+
+
+    public class Car : Vehicle
+    {
+        public override string Go() => "car says aron-don-don";
+    }
+
+    public class Ship : Vehicle
+    {
+        public override string Go() => "ship sound";
+    }
+
+    public class Plane : Vehicle
+    {
+        public override string Go() => "plane sound";
+    }
+
+}
